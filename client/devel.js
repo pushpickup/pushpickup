7d90e494233a0d38612556b9fb299aafbe089062
Donny Winston <dwinston@alum.mit.edu>
2014-04-05 19:05:44 -0700

    Omit privacy policy link from add/edit game views
    Close #82

--- client/devel.js
+++ client/devel.js
@@ -2101,3 +2101,13 @@ Template.devSubscriptions.helpers({
     return UserSubs.find().count();
   }
 });
+
+Template.devFooter.helpers({
+  omit: function () {
+    var self = this;
+    // Use data contexts supplied by add-game and edit-game routes
+    // to omit footer (i.e., Privacy Policy) for those views.
+    return (self.action && self.action === 'edit') ||
+      (self.action && self.action === 'add');
+  }
+});
