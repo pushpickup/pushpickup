06416deb9498b59d85c89db2fe8ff52068666014
Johannes Ma <hi@johannesma.com>
2014-04-11 12:16:01 -0700

    quick bug fixes.

--- client/devel.js
+++ client/devel.js
@@ -1078,6 +1078,12 @@ Template.devDetail.events({
   "click .share-game-link a": function () {
     Session.set("copy-game-link", this._id);
   },
+  "click .copy-game-link input": function () {
+    var copyGameLink = document.getElementById("copyGameLink")
+    copyGameLink.selectionStart = 0;
+    copyGameLink.selectionEnd = 999;
+    copyGameLink.readOnly = true;
+  },
   "click .copy-game-link .close": function () {
     Session.set("copy-game-link", null);
   },
