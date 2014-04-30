29c04c89d01517e429a06ee93054bde4b77d6d6b
tiz <tizhao@gmail.com>
2014-04-30 12:20:55 -0700

    minor code clean-up

--- client/devel.js
+++ client/devel.js
@@ -605,8 +605,13 @@ Template.inviteFriendsInput.helpers({
 });
 
 Template.listedGame.helpers({
-  organizing: function () {
-    return this.creator.userId == Meteor.userId();
+  organizingOrPlaying: function () {
+    var game = this;
+    var isPlaying = Games.findOne({_id: game._id, 'players.userId': Meteor.userId()})
+    var isOrganizing = this.creator.userId == Meteor.userId();
+    if (isPlaying || isOrganizing)
+      return true
+    return false
   }
 });
 
